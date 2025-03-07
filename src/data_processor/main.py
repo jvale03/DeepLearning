e76ac4f7b3ebec81b81c5f94a2b55cab1b7bc303
hramos04 <hugoantonioramos@hotmail.com>
2025-03-07 19:22:12 +0000

    main processamento dados

---
+++ src/data_processor/main.py
@@ -0,0 +1,24 @@
+import subprocess
+
+scripts = [
+    "merge_datasets.py",
+    "split_datasets.py",
+    "data_sample.py",
+    "process_text.py"
+]
+
+
+def executarScripts():
+    for script in scripts:
+        print(f"A executar {script}...")
+        result = subprocess.run(["python3", script], capture_output=True, text=True)
+        
+        print(result.stdout)
+        
+        if result.returncode != 0:
+            print(f"Erro ao executar {script}:\n{result}")
+            break
+
+
+if __name__ == "__main__":
+    executarScripts()
